8238c7e68bfabd32879ed6bc981237398b9ab23c
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2024-04-08 23:20:53 +0200

    added unit test file

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+namespace UnitTests;
+
+public class Tests
+{
+    
+    
+    
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.Fail();
+    }
+}
