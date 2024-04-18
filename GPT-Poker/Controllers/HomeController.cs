c1308383fba6613434992535c7df49b8e03e553e
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2024-04-18 11:15:34 +0200

    added login page

--- GPT-Poker/Controllers/HomeController.cs
+++ GPT-Poker/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GPT_Poker.Models;
 using LogicLayer;
-using LogicLayer.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace GPT_Poker.Controllers;
 
@@ -10,6 +10,21 @@ public class HomeController(ILogger<HomeController> logger) : Controller
 {
     private readonly ILogger<HomeController> _logger = logger;
 
+    
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!HttpContext.Session.TryGetValue("user", out _))
+        {
+            context.Result = new RedirectToRouteResult(new RouteValueDictionary{{ "controller", "Login" },  
+                { "action", "Index" }  
+            });  
+        }
+        
+        base.OnActionExecuting(context);
+    }
+    
+    
+    
     public IActionResult Index()
     {
         var view = View();
