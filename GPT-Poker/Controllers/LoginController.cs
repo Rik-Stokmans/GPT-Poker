4cb7d37e977f49aecb5e76648a30e449e6eefeb6
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2024-04-18 11:24:44 +0200

    added BaseController.cs

--- GPT-Poker/Controllers/LoginController.cs
+++ GPT-Poker/Controllers/LoginController.cs
@@ -1,26 +1,11 @@
 using LogicLayer;
 using LogicLayer.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace GPT_Poker.Controllers;
 
-public class LoginController : Controller
+public class LoginController : BaseController
 {
-    public override void OnActionExecuting(ActionExecutingContext context)
-    {
-        if (context.RouteData.Values["Action"] as string == "Logout") return;
-        if (HttpContext.Session.TryGetValue("user", out _))
-        {
-            context.Result = new RedirectToRouteResult(new RouteValueDictionary{{ "controller", "Home" },  
-                { "action", "Index" }  
-  
-            });  
-        }
-        
-        base.OnActionExecuting(context);
-    }
-    
     
     public IActionResult Index()
     {
